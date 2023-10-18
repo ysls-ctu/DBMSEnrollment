7f6e1f6f47cfd60dddb39eaea6227348bf29b2a9
hp <hp@DESKTOP-N22ISGA>
2023-10-18 09:10:31 +0800

    Add project files.

---
+++ DBMSEnrollment/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace DBMSEnrollment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdmin = new System.Windows.Forms.Button();
+            this.btnStudent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 49.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(153, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1002, 190);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "STUDENT\r\nENROLLMENT SYSTEM";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnAdmin.Font = new System.Drawing.Font("Lucida Bright", 16.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdmin.Location = new System.Drawing.Point(306, 361);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(318, 96);
+            this.btnAdmin.TabIndex = 1;
+            this.btnAdmin.Text = "ADMIN";
+            this.btnAdmin.UseVisualStyleBackColor = false;
+            // 
+            // btnStudent
+            // 
+            this.btnStudent.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnStudent.Font = new System.Drawing.Font("Lucida Bright", 16.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStudent.Location = new System.Drawing.Point(658, 361);
+            this.btnStudent.Name = "btnStudent";
+            this.btnStudent.Size = new System.Drawing.Size(318, 96);
+            this.btnStudent.TabIndex = 1;
+            this.btnStudent.Text = "STUDENT";
+            this.btnStudent.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1311, 650);
+            this.Controls.Add(this.btnStudent);
+            this.Controls.Add(this.btnAdmin);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "School Enrollment System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdmin;
+        private System.Windows.Forms.Button btnStudent;
+    }
+}
+
